7dc14a1ddd60f97915b91d5e48449bbb17bb8f4e
kim hyeon min <gus12als@gmail.com>
2022-04-14 15:26:42 +0900

    computer Homework udp_cheat

--- udp_cheating_client.c
+++ udp_cheating_client.c
@@ -1,99 +1,127 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+ 
+# define PORT 20162
+# define BUFFER_SIZE 4096 // 서버에 이용
+# define BUFF_SIZE 100 
 
-#define MAXLINE  511 
-#define BLOCK 255 
-
-struct sockaddr_in servaddr;
-int addrlen = sizeof(servaddr); 
-
-
-void sendMessage(int s, char* buf) {
-    if((sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&servaddr, addrlen)) < 0) {
-        perror("sendto fail");
+int main(int argc, char* argv[])
+{
+    /*
+    if(argc != 2)
+    {    // argv[0]에 ./client가 들어간다.
+        printf("사용법 : %s IPv4-address\n", argv[0]);
+        return -1;
+    }
+    */
+ 
+    int client_socket;
+    struct sockaddr_in serverAddress;
+    int server_addr_size;
+    char sendBuff[BUFF_SIZE];
+    char readBuff[BUFF_SIZE];
+ 
+ 
+    ssize_t receivedBytes = 0;
+    ssize_t sentBytes;
+ 
+ 
+    memset(&serverAddress, 0, sizeof(serverAddress));
+ 
+    serverAddress.sin_family = AF_INET;
+    inet_aton("127.0.0.1", (struct in_addr*) &serverAddress.sin_addr.s_addr);
+    serverAddress.sin_port = htons(20162);
+ 
+    // 소켓을 생성
+    if((client_socket = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        printf("socket 생성 실패\n");
         exit(0);
     }
-}
-
-int main(int argc, char *argv[]) {
-    int s; 
-    int nbyte;
-    char buf[MAXLINE+1], buf2[MAXLINE+1];
-    FILE *stream;
-
     
-    if(argc != 4) {
-        printf("usage: %s ip_address port_number filename\n", argv[0]);
-        exit(0);
+ 
+    // 닉네임 설정 시 닉네임과 암호키 100을 전송한다.
+    printf("닉네임을 설정하세요 : ");
+    char nickname[10] = {0};
+    fgets(nickname,10,stdin);
+    sendto(client_socket, nickname, 100, 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+ 
+    // 채팅유저인지 아닌지 설정한다.
+    int canChat = 0;
+    
+    // 채팅가능 판별을 위해 기다린다.
+    while((receivedBytes = recvfrom(client_socket, readBuff, BUFF_SIZE, 0, (struct sockaddr*)&serverAddress, &server_addr_size)) == 0){}
+ 
+    // 채팅 가능하면 메시지 출력
+    if(receivedBytes == 100)
+    {
+        readBuff[receivedBytes] = '\0';
+        fputs(readBuff, stdout);
+        fflush(stdout);
+        canChat = 1;
     }
-
-    
-    if((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket fail");
-        exit(0);
+ 
+    // 채팅 불가능하면 메시지 출력
+    else if(receivedBytes == 99)
+    {
+        readBuff[receivedBytes] = '\0';
+        fputs(readBuff, stdout);
+        fflush(stdout);
+        canChat = 0;
     }
-    
-    
-    memset(&servaddr, 0, addrlen); 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr(argv[1]); 
-    servaddr.sin_port = htons(atoi(argv[2])); 
-
-	if((stream = fopen(argv[3], "r")) == NULL) { 
-		printf("Error");
-	 	exit(1);
-	}
-
-    
-	while(!feof(stream)) {
-		buf[0] = '\0';
-		fgets(buf, BLOCK, stream); 
-
-		printf("Send : %s\n", buf); 
+ 
+    // 방이 꽉찬경우
+    else if(receivedBytes == 98)
+    {
+        readBuff[receivedBytes] = '\0';
+        fputs(readBuff, stdout);
+        fflush(stdout);
+        return 0;
+    }
+ 
+    while(canChat == 1)
+    {
+        // 채팅 프로그램 제작
+        server_addr_size = sizeof(serverAddress);
         
-   		sendMessage(s, buf);
-	}
-	fclose(stream);
-
-    sendMessage(s, "end of file"); 
-
+        //클라이언트에서 메세지 전송
+        printf("클라이언트에서 보낼 말을 입력하세요 :: ");
     
-	if((stream = fopen(argv[3], "r")) == NULL) {
-		printf("Error not File");
-		exit(1);
-	}
+        char msg[BUFF_SIZE];
+        fgets(msg,BUFF_SIZE,stdin);
+ 
+        // exit를 입력하면 98바이트 암호키를 통해 종료한다.
+        if(strcmp(msg,"exit\n") == 0)
+        {
+            sentBytes = sendto(client_socket, "Chatter is Logout\n", 98, 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+ 
+            return 0;
+        }
+ 
+        sprintf(sendBuff,"%s",msg);
+ 
+        sentBytes = sendto(client_socket, sendBuff, strlen(sendBuff), 0, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+ 
+        fflush(stdout);
+    }
+    while(canChat == 0)
+    {
+        receivedBytes = recvfrom(client_socket, readBuff, BUFF_SIZE, 0, (struct sockaddr*)&serverAddress, &server_addr_size);
+        readBuff[receivedBytes] = '\0';
+        fputs(readBuff, stdout);
+        fflush(stdout);
+        canChat = 0;
+        
+    }
     
-	while(!feof(stream))
-	{
-		buf2[0] = '\0'; 
-		fgets(buf2, BLOCK, stream); 
-		puts("get Server : waiting request.");
-        sendMessage(s, buf);
-		if((nbyte = recvfrom(s, buf, MAXLINE, 0, 
-				(struct sockaddr *)&servaddr, &addrlen)) < 0) {
-			perror("recvfrom fail");
-			exit(1);
-		}
-		buf[nbyte] = 0;
-
-		if(strncmp(buf, buf2, BLOCK)) { 
-			printf("Not Match buf : %s\nbuf2 : %s", buf, buf2);
-			fclose(stream);
-			exit(0);
-		} else {
-			printf("Match buf : %s\nbuf2 : %s", buf, buf2);
-		}
-		
-		puts("sendto complete");
-	}
-    
-	fclose(stream); 
-    close(s); 
+    // 소켓을 close 
+    close(client_socket);
     return 0;
 }
+ 
+
--- udp_cheating_server.c
+++ udp_cheating_server.c
@@ -1,88 +1,170 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+ 
+# define PORT 20162
+# define BUFFER_SIZE 4096 // 서버에 이용
+# define BUFF_SIZE 100
 
-#define MAXLINE    511
-#define BLOCK      255
-#define FILENAME "buf.txt"
-
-int main(int argc, char *argv[]) {
-    struct sockaddr_in servaddr, cliaddr;
-    int s, nbyte, addrlen = sizeof(struct sockaddr);
-    char buf[MAXLINE+1];
-	FILE *stream; 
+int main(int argc, char *argv[])
+{
+    char readBuff[BUFFER_SIZE];
+    char sendBuff[BUFFER_SIZE];
+    struct sockaddr_in serverAddress, clientAddress;
+    int server_fd, client_fd;
+    int client_addr_size;
+    ssize_t receivedBytes;
+    ssize_t sentBytes;
+ 
+    /*
+    if (argc != 2)
+    {
+        printf("사용법 : ./filename 포트번호 \n");
+        exit(0);
+    }
+    */
+ 
+    socklen_t clientAddressLength =0;
+ 
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    memset(&clientAddress, 0, sizeof(clientAddress));
+ 
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(20162);
+ 
+ 
+    // 서버 소켓 생성 및 서버 주소와 bind
     
-   
-    if(argc != 2) { 
-        printf("usage: %s port\n", argv[0]);
+    // 서버 소켓 생성(UDP니 SOCK_DGRAM이용)
+    if((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) // SOCK_DGRAM : UDP
+    {
+        printf("Sever : can not Open Socket\n");
+        exit(0);
+    }
+ 
+    // bind 과정
+    if(bind(server_fd, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        printf("Server : can not bind local address");
         exit(0);
     }
     
-    
-    if((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket fail");
-        exit(0);
-    }
-    
-    
-    memset(&cliaddr, 0, addrlen); 
-    memset(&servaddr, 0, addrlen); 
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(atoi(argv[1])); 
-
-    
-    if(bind(s, (struct sockaddr *)&servaddr, addrlen) < 0) {
-        perror("bind fail");
-        exit(0);
-    }
-    
-   
-	if((stream = fopen(FILENAME, "w")) == 0) {
-        printf("Faile open error\n");
-        exit(1);
-    }
+ 
+    printf("Server: waiting connection request.\n");
+ 
+    char nickname[5][10];
+    int chatter = 0;
+    int portArr[5] = {-1,-1,-1,-1,-1};
+    int pNum = 0;
+    int i = 0;
+ 
     while(1)
     {
-        puts("Server : waiting request.");
         
-        nbyte = recvfrom(s, buf, MAXLINE , 0, (struct sockaddr *)&cliaddr, &addrlen);
-        if(nbyte< 0) {
-            perror("recvfrom fail");
-            exit(1);
+    
+    //채팅 프로그램 제작
+    client_addr_size = sizeof(clientAddress);
+ 
+    receivedBytes = recvfrom(server_fd, readBuff, BUFF_SIZE, 0, (struct sockaddr*)&clientAddress, &client_addr_size);
+ 
+    // 닉네임을 닉네임 배열에 저장해준다.
+    readBuff[receivedBytes] = '\0';
+    sprintf(nickname[pNum],"%s",readBuff);
+    fflush(stdout);
+ 
+        // 닉네임 수신 할 때 100바이트를 수신한다.
+        if(receivedBytes == 100)
+        {
+            // 채팅 유저가 없으면 100바이트라는 임시 암호를 보낸다.
+            if(chatter == 0)
+            {
+                sentBytes = sendto(server_fd, "You can 'send Message'\n", 100, 0, (struct sockaddr*)&clientAddress, sizeof(clientAddress));
+                chatter = 1;                
+ 
+                int pt = clientAddress.sin_port;
+                portArr[0] = pt;
+                pNum = 0;
+                printf("Login Succeed ! \n Client : %s port[%d]\n\n",nickname[pNum], pt);
+                pNum++;
+    
+            }
+    
+            // 채팅 유저가 있으면 99바이트라는 임시 암호를 보낸다.
+            else if(chatter == 1)
+            {
+                
+                
+                int pt = clientAddress.sin_port;
+ 
+                for(i = 1;; i++)
+                {
+                    if(i <= 3 && portArr[i] == -1)
+                    {
+                        portArr[i] = pt;
+                        printf("Login Succeed ! \n Client :: %s port :: %d\n",nickname[i], portArr[i]);
+                        pNum++; // 닉네임 넘버를 위해 넣어둔다.
+                        break;
+                    }
+                    // 채팅방이 꽉 찼으면 거절한다.
+                    else if(i > 4)
+                    {
+                        sentBytes = sendto(server_fd, "This chatting room is full\n", 98, 0, (struct sockaddr*)&clientAddress, sizeof(clientAddress));
+                        break;
+                    }
+                }
+ 
+                sentBytes = sendto(server_fd, "You can't send Message 'Only hear'\n", 99, 0, (struct sockaddr*)&clientAddress, sizeof(clientAddress));
+            }
         }
-        buf[nbyte] = 0; 
-	
-        if(!strncmp(buf, "quit", 10)) { 
-            printf("Chat Closed");
-            fclose(stream); 
-            break; 
-        } else {
-        	printf("%d byte recv: %s\n",nbyte, buf);
-            fputs(buf, stream); 
+ 
+        // Chatter이 접속을 종료하면 배열 및 변수를 초기화해준다.
+        else if(receivedBytes == 98)
+        {
+            chatter = 0;
+            portArr[0] = -1;
+            nickname[0][0] = '\0';
+ 
+            readBuff[receivedBytes] = '\0';
+            fputs(readBuff, stdout);
+            fflush(stdout);
+ 
+            sprintf(sendBuff,"%s",readBuff);
+            
+            // 다른 클라이언트에게도 종료를 알려준다.
+            for(i = 1; portArr[i] != -1; i++)
+            {
+                clientAddress.sin_port = portArr[i];
+                sentBytes = sendto(server_fd, sendBuff, strlen(sendBuff), 0, (struct sockaddr*)&clientAddress, sizeof(clientAddress));
+            }
         }
-        puts("sendto complete");
+        
+        // 그 외에는 문자를 수신받는다.
+        else
+        {
+            printf("%lu bytes read\n", receivedBytes);
+            readBuff[receivedBytes] = '\0';
+            fputs(readBuff, stdout);
+            fflush(stdout);
+    
+            sprintf(sendBuff,"%s",readBuff);
+ 
+            // 현재 로컬 주소인 127.0.0.1에서 행동중이니 고유 포트 번호를 변경해주며 이용한다.
+            for(i = 1; portArr[i] != -1; i++)
+            {
+                clientAddress.sin_port = portArr[i];
+                sentBytes = sendto(server_fd, sendBuff, strlen(sendBuff), 0, (struct sockaddr*)&clientAddress, sizeof(clientAddress));
+            }
+            
+        }
     }
-	if((stream = fopen(FILENAME, "r")) == NULL) {
-		printf("Read File Error");
-		exit(1);
-	}
-
-	while(!feof(stream)) {
-		buf[0] = '\0';
-		fgets(buf, BLOCK, stream);
-		printf("Send : %s\n", buf);
-      
-		if(sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&cliaddr, addrlen) < 0) {
-			perror("sendto fail");
-			exit(0);
-		}
-	}
-	fclose(stream);
-    close(s);
-	return 0;
+ 
+    // 서버 소켓 close 
+    close(server_fd);
+ 
+    return 0;
 }
